ebde3774a77cb4da7d63ef4335e53af9deae6eb0
Захаров Николай Владимирович <palankar3@yandex.ru>
2026-01-08 19:59:41 +0300

    Передвижение персонажа по направлению камеры, повороты

--- Assets/Scripts/Test/Gameplay/Character/SimpleMovementCharacterController.cs
+++ Assets/Scripts/Test/Gameplay/Character/SimpleMovementCharacterController.cs
@@ -9,14 +9,18 @@ namespace Test
     public class SimpleMovementCharacterController : MonoBehaviour
     {
         public float speed = 0.5f;
+        public float rotationSpeed = 15f;
 
         private CharacterController _controller;
+        private Transform _cameraTransform;
         
         [SerializeField] private GameModeManager gameModeManager;
 
         void Awake()
         {
             if (gameModeManager == null) Debug.LogError("GameModeManager не назначен!");
+            if (_cameraTransform == null && Camera.main != null) _cameraTransform = Camera.main.transform;
+            if (_cameraTransform == null) Debug.LogError("Camera Transform не назначен!");
         }
         
         void Start()
@@ -34,12 +38,20 @@ namespace Test
         
         private void MoveCharacter()
         {
+            if (Input.GetMouseButton(1))
+            {
+                float mouseX = Input.GetAxis("Mouse X");
+                _cameraTransform.RotateAround(transform.position, Vector3.up, mouseX * rotationSpeed);
+            }
+            
             // Получение ввода с клавиатуры
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
 
             // Направление движения
-            Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
+            Vector3 moveInput = new Vector3(horizontal, 0, vertical);
+            Vector3 moveDirection = _cameraTransform.TransformDirection(moveInput);
+            moveDirection.y = 0;
             moveDirection = transform.TransformDirection(moveDirection);
 
             // Простое движение
